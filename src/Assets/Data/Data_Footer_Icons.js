a93e7c01c8b305524d3e405e204fff3551c24bdf
Abolfazl-Taj <leviackermanam18@gmail.com>
2024-09-22 17:28:18 +0330

    Updates

--- src/Assets/Data/Data_Footer_Icons.js
+++
@@ -1,6 +0,0 @@
-import { FaTwitter , FaLinkedinIn , FaPinterestP , FaFacebookF , FaInstagram  } from "react-icons/fa";
-
-const Data_Footer_Icons = [ <FaTwitter/> , <FaFacebookF/> , <FaInstagram/> , <FaPinterestP/> , <FaLinkedinIn/>    ]
-
-
- export default  Data_Footer_Icons
